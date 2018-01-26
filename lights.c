09f9d60dce392b6f81863066def98b9cd420873f
Rafal Strzalinski <nablaone@gmail.com>
2018-01-26 22:06:16 +0100

    Use TIMER1 instead of busy wait loop.

--- lights.c
+++ lights.c
@@ -1,10 +1,18 @@
 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 
 #define LED1 PB3
 #define LED2 PB2
-#define DELAY 250
+
+
+
+ISR(TIMER1_OVF_vect)
+{
+    PORTB ^= _BV(LED1) | _BV(LED2);
+}
 
 void delay_ms(uint8_t ms) {
     uint16_t delay_count = 1000000 / 17500;
@@ -17,15 +25,26 @@ void delay_ms(uint8_t ms) {
 }
 
 int main(void) {
-    //CKSEL = 8;
-    //int8_t i;
+    
+    cli();
+    
+    // clk/8 
+    TCCR1B |= (1 << CS11);
+  
+    // initialize counter
+    TCNT1 = 0;
+  
+    // enable overflow interrupt
+    TIMSK |= (1 << TOIE1);
+
+    sei(); 
+    
     DDRB = _BV(LED1) | _BV(LED2);
+    // 
+    PORTB = _BV(LED2);
     
     for(;;) {
-        PORTB = _BV(LED1);
-        delay_ms(DELAY);
-        PORTB = _BV(LED2);
-        delay_ms(DELAY);
+        sleep_cpu();
     }
     
     return 0;
