cf406288d2b7b671f1dfec244277436e33d1e007
Rafal Strzalinski <nablaone@gmail.com>
2018-10-15 21:27:41 +0200

    Added more example data

--- example/main.go
+++ example/main.go
@@ -16,20 +16,37 @@ func must(errs ...error) {
 	}
 }
 
+func allClients(db *gorm.DB) {
+	var clients []Client
+	must(db.Find(&clients).GetErrors()...)
+
+	for _, c := range clients {
+		fmt.Println("* ", c.ID, c.FullName, c.Email)
+		clientPurchase(db, &c)
+		fmt.Println()
+	}
+}
+
+func clientPurchase(db *gorm.DB, client *Client) {
+
+	var purchases []Purchase
+
+	must(db.Where("client_id = ?", client.ID).Find(&purchases).GetErrors()...)
+
+	for _, p := range purchases {
+		fmt.Println("  ", p.PurchaseNo)
+	}
+}
+
 func main() {
 	fmt.Println("Start")
 	db, err := gorm.Open("postgres", "host=localhost port=5432 user=test dbname=test password=test")
 	must(err)
 
-	db.LogMode(true)
+	//db.LogMode(true)
 	db.SingularTable(true)
 
-	var clients []Client
-	must(db.Find(&clients).GetErrors()...)
-
-	for _, c := range clients {
-		fmt.Println(c.ID, c.FullName, c.Email)
-	}
+	allClients(db)
 
 	defer db.Close()
 	fmt.Println("Done")
